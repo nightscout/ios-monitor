a24a49c4ddcdb0fc90dd6406da397f0a2c5773c3
jcostik <hackingtype1@gmail.com>
2014-12-08 13:25:20 -0500

    Initial commit from local git

---
+++ Nighscout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nighscout
+//
+//  Created by John Costik on 12/8/14.
+//  Copyright (c) 2014 Nightscout Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Nighscout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Nighscout
+//
+//  Created by John Costik on 12/8/14.
+//  Copyright (c) 2014 Nightscout Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
